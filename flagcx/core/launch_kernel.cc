4701b8416417527770188707e8008749b75fd142
MC952-arch <MC952-arch@qq.com>
2025-12-01 23:28:50 +0800

    Fix several bugs

--- flagcx/core/launch_kernel.cc
+++ flagcx/core/launch_kernel.cc
@@ -26,7 +26,39 @@ flagcxResult_t loadKernelSymbol(const char *path, const char *name,
 
 FLAGCX_PARAM(SemaphoreBufferPoolCapacity, "SEMAPHORE_BUFFER_POOL_CAPACITY", 32);
 
-flagcxDeviceSemaphoreBufferPool::flagcxDeviceSemaphoreBufferPool() {
+flagcxDeviceSemaphoreBufferPool::flagcxDeviceSemaphoreBufferPool()
+    : capacity(-1), slotId(-1), signalsPool(nullptr), dSignalsPool(nullptr),
+      events(nullptr) {}
+
+flagcxDeviceSemaphoreBufferPool::~flagcxDeviceSemaphoreBufferPool() {
+  free(events);
+  dSignalsPool = nullptr;
+  if (signalsPool != nullptr) {
+    deviceAdaptor->deviceFree((void *)signalsPool, flagcxMemHost, nullptr);
+  }
+}
+
+int flagcxDeviceSemaphoreBufferPool::getSlotId() {
+  assert(capacity != -1);
+  if (events[slotId] != nullptr) {
+    // wait for the previous event to complete
+    while (deviceAdaptor->eventQuery(events[slotId]) != flagcxSuccess) {
+      sched_yield();
+    }
+    events[slotId] = nullptr;
+  }
+  // set this slot signals to zero
+  int offset = FLAGCX_SIGNALS_PER_SEMAPHORE * slotId;
+  signalsPool[offset + FLAGCX_SIGNAL_START_OFFSET] = 0;   // started or not
+  signalsPool[offset + FLAGCX_SIGNAL_END_OFFSET] = 0;     // ended or not
+  signalsPool[offset + FLAGCX_SIGNAL_COUNTER_OFFSET] = 0; // total operations
+  int ret = slotId;
+  // Move to next slot
+  slotId = (slotId + 1) % capacity;
+  return ret;
+}
+
+void flagcxDeviceSemaphoreBufferPool::initialize() {
   capacity = flagcxParamSemaphoreBufferPoolCapacity();
   slotId = 0;
   // Allocate host-pinned memory for all semaphores (3 ints each)
@@ -43,43 +75,20 @@ flagcxDeviceSemaphoreBufferPool::flagcxDeviceSemaphoreBufferPool() {
   }
 }
 
-flagcxDeviceSemaphoreBufferPool::~flagcxDeviceSemaphoreBufferPool() {
-  free(events);
-  dSignalsPool = nullptr;
-  deviceAdaptor->deviceFree((void *)signalsPool, flagcxMemHost, nullptr);
-}
-
-int flagcxDeviceSemaphoreBufferPool::getSlotId() {
-  if (events[slotId] != nullptr) {
-    // wait for the previous event to complete
-    while (deviceAdaptor->eventQuery(events[slotId]) != flagcxSuccess) {
-      sched_yield();
-    }
-    events[slotId] = nullptr;
-  }
-  // set this slot signals to zero
-  int offset = FLAGCX_SIGNALS_PER_SEMAPHORE * slotId;
-  signalsPool[offset + FLAGCX_SIGNAL_START] = 0;   // started or not
-  signalsPool[offset + FLAGCX_SIGNAL_END] = 0;     // ended or not
-  signalsPool[offset + FLAGCX_SIGNAL_COUNTER] = 0; // total operations
-  int ret = slotId;
-  // Move to next slot
-  slotId = (slotId + 1) % capacity;
-  return ret;
-}
-
 // Set event for a semaphore
 void flagcxDeviceSemaphoreBufferPool::setEvent(int id, flagcxEvent_t event) {
+  assert(id >= 0 && id < capacity);
   // events[id] should be set to nullptr before
   events[id] = event;
-  return;
 }
 
 // Return pointer to the start of a semaphore’s signals (host/device)
 int *flagcxDeviceSemaphoreBufferPool::getHostPtr(int id) {
+  assert(id >= 0 && id < capacity);
   return signalsPool + FLAGCX_SIGNALS_PER_SEMAPHORE * id;
 }
 void *flagcxDeviceSemaphoreBufferPool::getDevicePtr(int id) {
+  assert(id >= 0 && id < capacity);
   return static_cast<void *>((static_cast<char *>(dSignalsPool) +
                               FLAGCX_SIGNALS_PER_SEMAPHORE * id * sizeof(int)));
 }
