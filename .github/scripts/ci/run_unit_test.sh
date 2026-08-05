37d2b7807d034c57cd9530b47bfbe85e9ca1190f
马函廷 <19025408700@163.com>
2026-08-05 19:05:20 +0800

    ci: align device api runner with upstream tests

--- .github/scripts/ci/run_unit_test.sh
+++ .github/scripts/ci/run_unit_test.sh
@@ -69,7 +69,7 @@ run_device_api() {
     -x FLAGCX_P2P_DISABLE=1
     -x LD_LIBRARY_PATH
   )
-  local -a flags=(-b 1M -e 4M -f 2 -R 2)
+  local -a flags=(-b 1M -e 4M -f 2 -R 1)
 
   declare -p FLAGCX_CI_NODE1_MPI_ARGS >/dev/null 2>&1 || {
     echo "The platform set_env script must define FLAGCX_CI_NODE1_MPI_ARGS" >&2
@@ -84,15 +84,15 @@ run_device_api() {
 
   cd "$suite_dir"
   mpirun -np "$FLAGCX_CI_INTRA_NP" --allow-run-as-root "${common_env[@]}" \
-    build/bin/test_device_api "${flags[@]}"
+    build/bin/test_device_api_intra "${flags[@]}"
   mpirun -np "$FLAGCX_CI_INTRA_NP" --allow-run-as-root "${common_env[@]}" \
     build/bin/test_device_ir_intra "${flags[@]}"
 
   mpirun --allow-run-as-root \
     -np "$FLAGCX_CI_NODE_NP" "${common_env[@]}" "${FLAGCX_CI_NODE1_MPI_ARGS[@]}" \
-    build/bin/test_device_api "${flags[@]}" \
+    build/bin/test_device_api_inter "${flags[@]}" \
     : -np "$FLAGCX_CI_NODE_NP" "${common_env[@]}" "${FLAGCX_CI_NODE2_MPI_ARGS[@]}" \
-    build/bin/test_device_api "${flags[@]}"
+    build/bin/test_device_api_inter "${flags[@]}"
   mpirun --allow-run-as-root \
     -np "$FLAGCX_CI_NODE_NP" "${common_env[@]}" "${FLAGCX_CI_NODE1_MPI_ARGS[@]}" \
     build/bin/test_device_ir_inter "${flags[@]}" \
