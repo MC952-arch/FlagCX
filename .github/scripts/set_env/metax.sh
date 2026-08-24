364f0887d5e0475310e8ce68e3abf8e303a36792
canghaiX <1395976031@qq.com>
2026-08-24 14:27:31 +0800

    Fix MetaX driver library paths in CI

--- .github/scripts/set_env/metax.sh
+++ .github/scripts/set_env/metax.sh
@@ -18,6 +18,7 @@ else
 fi
 
 export PATH="/opt/maca/mxgpu_llvm/bin:$PATH"
+export LD_LIBRARY_PATH="/opt/mxdriver/lib:/opt/maca/lib:/usr/local/lib:${LD_LIBRARY_PATH:-}"
 
 FLAGCX_CI_PROJECT_MAKE_ARGS=(USE_METAX=1)
 FLAGCX_CI_TEST_MAKE_ARGS=(USE_METAX=1)
